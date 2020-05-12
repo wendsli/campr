136c840d3405228f2eb1906a26badde6a8e59ce5
wendsli <wesley.adams.davis@gmail.com>
2020-05-12 01:50:32 -0400

    updated show page callout to allow for additional page content

--- app/javascript/react/components/CampgroundShowTile.js
+++ app/javascript/react/components/CampgroundShowTile.js
@@ -55,100 +55,24 @@ const CampgroundShowTile = (props) => {
     <label>Waste Disposal</label>
   </div>
 
-  // if (camp.store) {
-  //   store = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="store" size="2x"
-  //   />
-  // } else {
-  //   store = <div className={`campground-form-icon-container
-  //     ${camp["store"] ? "selected" : "unselected" }`}>
-  //     <FontAwesomeIcon
-  //       className="campground-icon unavailable-service-campground-icon"
-  //       icon="store" size="2x"
-  //     />
-  //     <label>Store</label>
-  //   </div>
-  // }
-
-  // if (camp.firewood) {
-  //   firewood = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="tree" size="2x"
-  //   />
-  // } else {
-  //   firewood = <FontAwesomeIcon
-  //     className="campground-icon unavailable-service-campground-icon"
-  //     icon="tree" size="2x"
-  //   />
-  // }
-  //
-  // if (camp.bathrooms) {
-  //   bathrooms = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="restroom" size="2x"
-  //   />
-  // } else {
-  //   bathrooms = <FontAwesomeIcon
-  //     className="campground-icon unavailable-service-campground-icon"
-  //     icon="restroom" size="2x"
-  //   />
-  // }
-  //
-  // if (camp.showers) {
-  //   showers = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="shower" size="2x"
-  //   />
-  // } else {
-  //   showers = <FontAwesomeIcon
-  //     className="campground-icon unavailable-service-campground-icon"
-  //     icon="shower" size="2x"
-  //   />
-  // }
-  //
-  // if (camp.utilities) {
-  //   utilities = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="charging-station" size="2x"
-  //   />
-  // } else {
-  //   utilities = <FontAwesomeIcon
-  //     className="campground-icon unavailable-service-campground-icon"
-  //     icon="charging-station" size="2x"
-  //   />
-  // }
-  //
-  // if (camp.waste) {
-  //   waste = <FontAwesomeIcon
-  //     className="campground-icon available-service-campground-icon"
-  //     icon="trash-alt" size="2x"
-  //   />
-  // } else {
-  //   waste = <FontAwesomeIcon
-  //     className="campground-icon unavailable-service-campground-icon"
-  //     icon="trash-alt" size="2x"
-  //   />
-  // }
-
   return (
     <div className="callout campground-show-tile">
       <div className="campground-overview grid-container grid-x">
-        <img className="campground-show-image" src={campgroundImage} />
+        <div className="campground-show-image-container">
+          <img className="campground-show-image" src={campgroundImage} />
+        </div>
         <div className="campground-info">
           <h5><a href={camp.website} target="_blank">{camp.name}</a></h5>
           <p>{camp.street}, {camp.city}, {camp.state}, {camp.zip}</p>
           <p><a href="tel:${camp.phone}">{camp.phone}</a></p>
           <p>{campgroundLatLong}</p>
-          <div className="campground-show-icons">
-            <div className="grid-x grid-padding-x">
-              {store}
-              {firewood}
-              {bathrooms}
-              {showers}
-              {utilities}
-              {waste}
-            </div>
+          <div className="campground-show-icons grid-x grid-padding-x">
+            {store}
+            {firewood}
+            {bathrooms}
+            {showers}
+            {utilities}
+            {waste}
           </div>
         </div>
       </div>
